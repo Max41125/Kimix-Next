236de362877d01730ed539aba244e1e4a2ba2313
Max41125 <desyatkov.maxim@gmail.com>
2024-10-01 23:45:05 +0500

    Автоматическая сборка

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 // src/app/page.js
-import Image from "next/image";
+
 import Header from "./components/Header"; // импортируем Header
-import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main >
       <Header /> 
 
     </main>
