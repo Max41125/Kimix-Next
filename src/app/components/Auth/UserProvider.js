ee2ff38a13b36be10e965218842829012f550021
Max41125 <desyatkov.maxim@gmail.com>
2024-10-13 12:29:02 +0500

    logout

--- src/app/components/Auth/UserProvider.js
+++ src/app/components/Auth/UserProvider.js
@@ -2,11 +2,14 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 const UserContext = createContext();
+import axios from 'axios';
+
 
 export const UserProvider = ({ children, csrfToken }) => {
     const [user, setUser] = useState(null);
     const [token, setToken] = useState('');
     const router = useRouter();
+    const csrfUrl = 'https://test.kimix.space/sanctum/csrf-cookie';
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -28,32 +31,39 @@ export const UserProvider = ({ children, csrfToken }) => {
     };
 
     const logout = async () => {
+        const url = 'https://test.kimix.space/api/auth/logout';
         try {
-            const response = await fetch('https://test.kimix.space/api/auth/logout', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-
-            if (response.ok) {
-                setUser(null);
-                setToken('');
-                localStorage.removeItem('token');
-                localStorage.removeItem('user');
-                console.log('Logout successful');
-                router.push('/');
-            } else {
-                const data = await response.json();
-                console.error('Logout error:', data);
-            }
+          // Запрос для получения CSRF токена
+          await axios.get(csrfUrl, {
+            withCredentials: true,
+          });
+      
+          // Запрос на логаут
+          const response = await axios.post(url, {}, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            withCredentials: true,
+          });
+      
+          // Проверка успешности запроса
+          if (response.status >= 200 && response.status < 300) {
+            setUser(null);
+            setToken('');
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            console.log('Logout successful');
+            router.push('/');
+          } else {
+            console.error('Logout error:', response.data);
+          }
         } catch (error) {
-            console.error('Logout request error:', error);
+          console.error('Logout request error:', error);
         }
-    };
-
+      };
+      
     return (
         <UserContext.Provider value={{ user, token, login, logout }}>
             {children}
