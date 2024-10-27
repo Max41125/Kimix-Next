2edc918573f6aba859bd492c19efb7fa0caaac11
Max41125 <desyatkov.maxim@gmail.com>
2024-10-28 02:37:49 +0500

    Suspense fix

--- src/app/dashboard/chat/contract/page.js
+++ src/app/dashboard/chat/contract/page.js
@@ -1,32 +1,42 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import RussianContract from '@/app/components/Chat/Contract/RussianContract';
 import EnglishContract from '@/app/components/Chat/Contract/EnglishContract';
+import Loader from '@/app/components/Loaders/Loader';
 
 const ContractPage = () => {
+  // Wrap useSearchParams in a Suspense boundary
+  return (
+    <Suspense fallback={<Loader />}>
+      <SearchParamsWrapper />
+    </Suspense>
+  );
+};
+
+// A separate component to handle search params and conditional rendering
+const SearchParamsWrapper = () => {
   const searchParams = useSearchParams();
   const orderId = searchParams.get('orderId');
   const lang = searchParams.get('lang');
   const currentDate = new Date().toLocaleDateString();
 
-  if (!orderId ||!lang) {
+  if (!orderId || !lang) {
     return <div>Error: Invalid parameters</div>;
   }
 
   return (
     <div className="container mx-auto p-6">
-         
-    {lang === 'ru' ? (
-      <h1 className="text-2xl font-bold mb-4">Интерактивный контракт</h1> ) : (
-      <h1 className="text-2xl font-bold mb-4">Interactive contract</h1> 
-   
+      {lang === 'ru' ? (
+        <h1 className="text-2xl font-bold mb-4">Интерактивный контракт</h1>
+      ) : (
+        <h1 className="text-2xl font-bold mb-4">Interactive contract</h1>
       )}
       {lang === 'ru' ? (
         <RussianContract orderId={orderId} currentDate={currentDate} />
       ) : (
-        <EnglishContract orderId={orderId} currentDate={currentDate}  />
+        <EnglishContract orderId={orderId} currentDate={currentDate} />
       )}
     </div>
   );
